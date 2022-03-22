d4dba08afb65de19dc08aa97fe4b97cd7394d48e
Exploration <zhangphil@outlook.com>
2022-03-22 16:07:13 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>House Pusher Privacy Policy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Info" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          House Pusher does not collect any data.
         </p>
       </main>
 
